1fb554d2388449d31bc4ccdce58d8e014d111f2f
ghockin <79321882+ghockin@users.noreply.github.com>
2023-05-04 08:23:51 +0100

    web scrap guardin news

--- com616/server/src/index.js
+++ com616/server/src/index.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import mongoose from "mongoose";
 import { userRouter } from "./routes/user.js";
 import { squadsRouter } from "./routes/squads.js";
+import axios from 'axios';
+import * as cheerio from 'cheerio';
 
 const app = express();
 
@@ -19,6 +21,26 @@ mongoose.connect(
         useUnifiedTopology: true,
     }
 );
+
+
+//Web Scrap
+const bbcNewsUrl= 'https://www.theguardian.com/sport/tennis'
+axios(bbcNewsUrl).then(response => {
+    const html = response.data;
+    const articles = [];
+    const $ = cheerio.load(html);
+    $('.fc-item__content ', html).each(function(){
+        const title = $(this).text();
+        const url = $(this).find('a');
+        articles.push({
+            title,
+            url,
+        })
+    })
+    articles.length = 4;
+    console.log(articles);
+}).catch(err => console.log(err))
+
 
 app.listen(3001, () => {
     console.log("Server is running")
